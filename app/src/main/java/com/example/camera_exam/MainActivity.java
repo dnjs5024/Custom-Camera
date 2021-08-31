29a9c64f5dfe6d8c62dccc0364def109a7e96f91
dnjs5024 <dnjs7469@naver.com>
2021-08-31 22:12:37 +0900

    0831

--- app/src/main/java/com/example/camera_exam/MainActivity.java
+++ app/src/main/java/com/example/camera_exam/MainActivity.java
@@ -17,19 +17,25 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -39,13 +45,17 @@ public class MainActivity extends AppCompatActivity {
     private  MainAdapter mainAdapter;
     private Button button;
     private GridLayoutManager gridLayoutManager;
-    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Location");
     private int last_num = 0;
+    private Map<String,Object> map ;
+    private long backBtnTime = 0;
+    private CheckBox checkBox;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //툴바생성
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -57,38 +67,50 @@ public class MainActivity extends AppCompatActivity {
         list_data = new ArrayList<MainData>();
         mainAdapter = new MainAdapter(list_data,this);
         recyclerView.setAdapter(mainAdapter);
-
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                int cnt = 0;
-                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    MainData mainData = snapshot.getValue(MainData.class);
-                    Log.e("snapshot",snapshot.getValue().toString());
+        
+        //기본 경로 만들기
+        MainData firstData = new MainData(R.mipmap.ic_launcher,"기본경로");
+        list_data.add(firstData);
+        mainAdapter.notifyDataSetChanged();
+        
+        //String ==> json ==> Map으로 변환
+        SharedPreferences sharedPreferences = getSharedPreferences("folderName",0);
+        String jMap = sharedPreferences.getString("jMap","");
+        Log.e("jMap",jMap);
+        try {
+            if(!jMap.equals("")){
+                JSONObject jsonObject = new JSONObject(jMap);
+                ObjectMapper objectMapper = new ObjectMapper();
+                map = objectMapper.readValue(jMap,Map.class);
+                Log.e("map",map.toString());
+                Iterator<String>iterator = map.keySet().iterator();
+                while (iterator.hasNext()){
+                    Object key = iterator.next();
+                    String value = map.get(key).toString();
+                    MainData mainData = new MainData(R.mipmap.ic_launcher,value);
                     list_data.add(mainData);
-                    cnt++;
+                    mainAdapter.notifyDataSetChanged();
+
                 }
-                last_num = cnt+1;
-                mainAdapter.notifyDataSetChanged();
+            }else{
+                map = new HashMap<>();
+                Log.e("map","비어있음");
             }
-
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.e("MainActivity", String.valueOf(error.toException()));
-            }
-        });
+        } catch (JSONException e) {
+            Log.e("String==>Json",e.toString());
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         button = (Button) findViewById(R.id.btn_add);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences sharedPreferences = getSharedPreferences("last_num",0);
-                last_num = sharedPreferences.getInt("last_num",0);
-                databaseReference.child("location_"+last_num).child("iv_profile").setValue("https://firebasestorage.googleapis.com/v0/b/camera-exam.appspot.com/o/test11.png?alt=media&token=2e18f25a-393f-468a-a31f-6c34ba0f5b2f");
-                databaseReference.child("location_"+last_num).child("tv_title").setValue("test");
-                last_num++;
+
             }
         });
 
@@ -123,7 +145,8 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     String result = editText.getText().toString();
-                    MainData mainData = new MainData("https://firebasestorage.googleapis.com/v0/b/camera-exam.appspot.com/o/test11.png?alt=media&token=2e18f25a-393f-468a-a31f-6c34ba0f5b2f", result);
+                    MainData mainData = new MainData(R.mipmap.ic_launcher, result);
+                    map.put(result,result);
                     list_data.add(mainData);
                     mainAdapter.notifyDataSetChanged();
                     dialog.dismiss();
@@ -132,24 +155,49 @@ public class MainActivity extends AppCompatActivity {
             ad.setNegativeButton("취소", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
+
                     dialog.dismiss();
                 }
             });
             ad.show();
             return true;
         }else if(id == R.id.menu_2){
+            //체크박스
+            checkBox = (CheckBox)findViewById(R.id.cb_delete);
             Toast.makeText(getApplicationContext(),"삭제",Toast.LENGTH_SHORT);
+            checkBox.setVisibility(View.VISIBLE);
+
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    //두번누르면 종료
+    @Override
+    public void onBackPressed() {
+
+        long currentTime = System.currentTimeMillis();
+        long gapTime = currentTime - backBtnTime;
+
+        if(0 <= gapTime && 2000>= gapTime ){
+            super.onBackPressed();
+        }else{
+            backBtnTime = currentTime;
+            Toast.makeText(getApplicationContext(),"한번 더 누르면 종료됩니다.",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        SharedPreferences.Editor editor = getSharedPreferences("last_num",0).edit();
-        editor.putInt("last_num",last_num);
+        JSONObject jsonObject = new JSONObject(map);
+        SharedPreferences sharedPreferences = getSharedPreferences("folderName",0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("jMap",jsonObject.toString());
         editor.commit();
+        Log.e("jMap",sharedPreferences.getString("jMap",""));
     }
 }
--- app/src/main/java/com/example/camera_exam/MainData.java
+++ app/src/main/java/com/example/camera_exam/MainData.java
@@ -2,23 +2,23 @@ package com.example.camera_exam;
 
 public class MainData {
 
-    private String iv_profile;
+    private int iv_profile;
     private String tv_title;
 
     public MainData(){
 
     }
 
-    public MainData(String iv_profile, String tv_title) {
+    public MainData(int iv_profile, String tv_title) {
         this.iv_profile = iv_profile;
         this.tv_title = tv_title;
     }
 
-    public String getIv_profile() {
+    public int getIv_profile() {
         return iv_profile;
     }
 
-    public void setIv_profile(String iv_profile) {
+    public void setIv_profile(int iv_profile) {
         this.iv_profile = iv_profile;
     }
 
